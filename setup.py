2c26c8ed3dd91a82026c55aead69743e39991203
VivekShinde7 <vivushinde02@gmail.com>
2024-09-26 13:56:20 +0530

    add requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "src",
+    version = "0.0.0",
+    author = "Vivek",
+    author_email="vivushinde02@gmail.com",
+    packages=find_packages()
+
+)
